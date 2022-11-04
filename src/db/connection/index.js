8fc43542c6117d2606d0b1f43e7ba4f8a9bdd776
NITESH <sahninitesh01@gmail.com>
2022-11-04 08:30:43 +0530

    updated

--- src/db/connection/index.js
+++ src/db/connection/index.js
@@ -3,26 +3,24 @@ const mongoose = require("mongoose")
 require("dotenv").config()
 
 
-try{
-    console.log("DB URL ====> ",process.env.MONGODB_URL)
-    if(process.env.MONGODB_URL){
-        mongoose.connect(process.env.MONGODB_URL);
-    }
+try {
+    mongoose.connect(process.env.MONGODB_URL);
+
 } catch (err) {
-    console.log("Database connection Error ====> ",err)
+    console.log("Database connection Error ====> ", err)
 }
 
-const conn =  mongoose.connection;
+const conn = mongoose.connection;
 
 
-conn.on("connecting",()=>{
+conn.on("connecting", () => {
     console.log("Database Connecting...")
 })
 
-conn.on("connected",()=>{
+conn.on("connected", () => {
     console.log("Database Connected Successfully")
 })
 
-conn.on("disconnected",()=>{
+conn.on("disconnected", () => {
     console.log("Database Disconnected")
 })
