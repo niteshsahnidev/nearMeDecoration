3c6a10280e5ecb320ccfaac8daf7226d7e07be39
NITESH <sahninitesh01@gmail.com>
2022-11-04 08:16:21 +0530

    testing

--- src/db/connection/index.js
+++ src/db/connection/index.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose")
 
-// require("dotenv").config()
+require("dotenv").config()
 
 
 try{
+    console.log("DB URL ====> ",process.env.MONGODB_URL)
     mongoose.connect(process.env.MONGODB_URL);
 } catch (err) {
     console.log("Database connection Error ====> ",err)
